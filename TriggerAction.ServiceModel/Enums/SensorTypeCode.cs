98176b77455ce435397490b52e5f7e079dbc41d6
Tito Brasolin <tito.brasolin@kerberos.energy>
2021-04-30 09:24:09 +0200

    doc: property descriptions

--- TriggerAction.ServiceModel/Enums/SensorTypeCode.cs
+++ TriggerAction.ServiceModel/Enums/SensorTypeCode.cs
@@ -1,14 +1,22 @@
-﻿namespace TriggerAction.ServiceModel.Enums
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace TriggerAction.ServiceModel.Enums
 {
     /// <summary>
     /// http://smartcityplatform.enea.it/specification/semantic/1.0/gc/SensorTypeCode.gc
     /// </summary>
     public enum SensorTypeCode
     {
+        [Description("Smart Plug")]
         SmartPlug = 1,
+        [Description("Smart Switch")]
         SmartSwitch = 2,
+        [Description("Smart Presence")]
         SmartPresence = 3,
+        [Description("Confort Multisensor")]
         ConfortMultisensor = 4,
+        [Description("Weather Multisensor")]
         WeatherMultisensor = 5,
     }
 }
