86ada238926faaef32094be73ba900b31332bf09
Tito Brasolin <tito.brasolin@kerberos.energy>
2021-05-19 15:26:30 +0200

    feat: new BasicRequest class

---
+++ TriggerAction.ServiceModel/Basic.cs
@@ -0,0 +1,25 @@
+﻿using ServiceStack;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TriggerAction.ServiceModel.Types;
+
+namespace TriggerAction.ServiceModel
+{
+    [Route("/basicRequest", "POST")]
+    public class BasicRequest : IReturn<BasicResponse>
+    {
+        [DataMember(Name = "resource_id")]
+        public string ResourceId { get; set; }
+    }
+
+    [DataContract]
+    public class BasicResponse
+    {
+        [DataMember(Name = "code")]
+        public string Code { get; set; }
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+        [DataMember(Name = "dataset")]
+        public List<Template> Dataset { get; set; }
+    }
+}
