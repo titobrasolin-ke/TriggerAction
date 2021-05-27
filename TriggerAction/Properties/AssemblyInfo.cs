70f3be92a29f6190e1fb9eebb0a26548973491f9
Tito Brasolin <tito.brasolin@kerberos.energy>
2021-05-27 14:30:56 +0200

    chore: update version

--- TriggerAction/Properties/AssemblyInfo.cs
+++ TriggerAction/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.5.1")]
-[assembly: AssemblyFileVersion("1.0.5.1")]
+[assembly: AssemblyVersion("1.0.9.1")]
+[assembly: AssemblyFileVersion("1.0.9.1")]
 [assembly: Guid("43671F59-404E-4470-B30C-5B2C21B8D668")]
